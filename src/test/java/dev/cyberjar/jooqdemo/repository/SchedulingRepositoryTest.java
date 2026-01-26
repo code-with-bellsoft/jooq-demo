586bab1c3d2d0bf6d6062563f77642a54621a9b4
Catherine Edelveis <ekaterina.edelveis@gmail.com>
2026-01-26 17:49:00 +0300

    add test for cte

---
+++ src/test/java/dev/cyberjar/jooqdemo/repository/SchedulingRepositoryTest.java
@@ -0,0 +1,37 @@
+package dev.cyberjar.jooqdemo.repository;
+
+import dev.cyberjar.jooqdemo.dto.SlotSuggestionDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
+@Rollback
+public class SchedulingRepositoryTest {
+
+    @Autowired
+    private SchedulingRepository schedulingRepository;
+
+    static PostgreSQLContainer postgres = new PostgreSQLContainer(
+            "postgres:16-alpine"
+    );
+
+    @Test
+    void shouldReturnSlotsWithCalculatedCapacity() {
+        List<SlotSuggestionDto> suggestions = schedulingRepository.suggestSlots(2L);
+
+        assertThat(suggestions).isNotEmpty();
+        assertThat(suggestions.getFirst().remainingCapacity()).isEqualTo(1);
+
+    }
+}
